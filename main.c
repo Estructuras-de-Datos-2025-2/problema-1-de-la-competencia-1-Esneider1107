f3ed3e4e2cf5a55f3126f529e4bf8aac2bff36af
Esneider1107 <esneiderjosue1107@gmail.com>
2025-08-29 09:41:17 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,14 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
-{
-    int n;
-    scanf("%d", &n);
-    // TODO: reservar arreglo dinamico de n enteros
-    // TODO: leer n enteros, calcular suma y promedio
-    // TODO: imprimir:
-    // Suma: <entero>
-    // Promedio: <real con 2 decimales>
-    // TODO: free
-    return 0;
+
+int main() {
+int n;
+int *p;
+
+scanf("%d", &n);
+p= (int*) malloc(n * sizeof(int));
+
+for (int i = 0; i < n; i++)
+    scanf("%d", p + i);
+
+for (int i = n -1; i >= 0; i--)
+    printf("%d ", *(p + i));
+
+
+free(p);
+return 0;    
 }
